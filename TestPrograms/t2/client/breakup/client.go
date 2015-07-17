55ee43fddfbd86b1864bda1e4ec0cf27563737e7
Wanton Solutions <stewbertgrant@gmail.com>
2015-07-17 16:30:10 -0700

    Test 2 modified to instrument multiple files in the same packages

--- TestPrograms/t2/client/breakup/client.go
+++ TestPrograms/t2/client/breakup/client.go
@@ -1,4 +1,4 @@
-package main
+package client
 
 import (
 	"fmt"
@@ -21,7 +21,7 @@ var (
 	term1, term2, sum int
 )
 
-func main() {
+func Client() {
 	//dump
 	Logger = govec.Initialize("Client", "testclient.log")
 	rAddr, errR := net.ResolveUDPAddr("udp4", ":8080")
@@ -52,50 +52,11 @@ func main() {
 
 		uret := UnmarshallInts(ret)
 		sum = uret[0]
-		fmt.Printf("C: %d + %d = %d\n", term1, term2, sum)
 		//@dump
+		fmt.Printf("C: %d + %d = %d\n", term1, term2, sum)
 		sum = 0
 	}
 	os.Exit(0)
-}
-
-func printErr(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-func MarshallInts(args []int) []byte {
-	var i, j uint
-	marshalled := make([]byte, len(args)*SIZEOFINT, len(args)*SIZEOFINT)
-	for j = 0; int(j) < len(args); j++ {
-		for i = 0; i < SIZEOFINT; i++ {
-			marshalled[(j*SIZEOFINT)+i] = byte(args[j] >> ((SIZEOFINT - 1 - i) * 8))
-		}
-	}
-	l := int(i)
-	k := int(j)
-	l = l + k
-	print(l)
-	//@dump
-	return marshalled
-}
-
-func UnmarshallInts(args []byte) []int {
-	var i, j uint
-	unmarshalled := make([]int, len(args)/SIZEOFINT, len(args)/SIZEOFINT)
-	for j = 0; int(j) < len(args)/SIZEOFINT; j++ {
-		for i = 0; i < SIZEOFINT; i++ {
-			unmarshalled[j] += int(args[SIZEOFINT*(j+1)-1-i] << (i * 8))
-		}
-	}
-	l := int(i)
-	k := int(j)
-	l = l + k
-	print(l)
-	//@dump
-	return unmarshalled
 }
 
 var Logger *govec.GoLog
---
+++ TestPrograms/t2/server/serverEntry.go
@@ -0,0 +1,7 @@
+package main
+
+import "bitbucket.org/bestchai/dinv/TestPrograms/t2/server/breakup"
+
+func main() {
+	server.Server()
+}
