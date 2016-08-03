a5b46b5f359e9d356296ad40e86bbca97bc2fb77
Stewart Grant <stewbertgrant@gmail.com>
2016-08-03 12:54:59 -0700

    update to the runscript for linn

--- examples/linear/client/client.go
+++ examples/linear/client/client.go
@@ -5,26 +5,23 @@ import (
 	"math/rand"
 	"net"
 	"os"
-
-	"github.com/arcaneiceman/GoVector/govec"
-	"bitbucket.org/bestchai/dinv/examples/linear/comm"
 )
 
 const (
 	ADDITION_ARGS = 2
 	LARGEST_TERM  = 100
-	RUNS          = 100
+	RUNS          = 50
 )
 
 var debug = false
 
 func main() {
 	rAddr, errR := net.ResolveUDPAddr("udp4", ":8080")
-	comm.PrintErr(errR)
+	PrintErr(errR)
 	lAddr, errL := net.ResolveUDPAddr("udp4", ":7071")
-	comm.PrintErr(errL)
+	PrintErr(errL)
 	conn, errDial := net.DialUDP("udp", lAddr, rAddr)
-	comm.PrintErr(errDial)
+	PrintErr(errDial)
 
 	var (
 		buf               [1024]byte
@@ -35,19 +32,19 @@ func main() {
 		fmt.Printf("\rExecuting[%2.0f]", float32(t)/float32(RUNS)*100)
 		term1, term2 = rand.Int()%LARGEST_TERM, rand.Int()%LARGEST_TERM
 
-		msg := comm.MarshallInts([]int{term1, term2})
+		msg := MarshallInts([]int{term1, term2})
 		// sending UDP packet to specified address and port
 		_, errWrite := conn.Write(msg)
 		//@dump
-		comm.PrintErr(errWrite)
+		PrintErr(errWrite)
 
 		// Reading the response message
 
 		_, errRead := conn.Read(buf[0:])
 		//@dump
-		comm.PrintErr(errRead)
+		PrintErr(errRead)
 
-		uret := comm.UnmarshallInts(buf)
+		uret := UnmarshallInts(buf)
 		sum = uret[0]
 		//if debug {
 		//	fmt.Printf("C: x*%d + %d = %d\n", term1, term2, sum)
@@ -57,5 +54,35 @@ func main() {
 	fmt.Println()
 	os.Exit(0)
 }
+const (
+	SIZEOFINT = 4
+)
 
-var Logger *govec.GoLog
+func PrintErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func MarshallInts(args []int) []byte {
+	var i, j uint
+	marshalled := make([]byte, len(args)*SIZEOFINT, len(args)*SIZEOFINT)
+	for j = 0; int(j) < len(args); j++ {
+		for i = 0; i < SIZEOFINT; i++ {
+			marshalled[(j*SIZEOFINT)+i] = byte(args[j] >> ((SIZEOFINT - 1 - i) * 8))
+		}
+	}
+	return marshalled
+}
+
+func UnmarshallInts(args [1024]byte) []int {
+	var i, j uint
+	unmarshalled := make([]int, len(args)/SIZEOFINT, len(args)/SIZEOFINT)
+	for j = 0; int(j) < len(args)/SIZEOFINT; j++ {
+		for i = 0; i < SIZEOFINT; i++ {
+			unmarshalled[j] += int(args[SIZEOFINT*(j+1)-1-i] << (i * 8))
+		}
+	}
+	return unmarshalled
+}
