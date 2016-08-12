4cf73e57370d0999a30cd222310337bc977a6083
Wanton Solutions <stewbertgrant@gmail.com>
2016-08-12 14:07:35 -0700

    added profiling tools for runtime enviornment

---
+++ dinvRT/api_test.go
@@ -0,0 +1,17 @@
+package dinvRT
+
+import "testing"
+
+var (
+	integer int
+	words string
+	boolean bool
+)
+
+func BenchmarkDumps(b *testing.B) {
+	for i:= 0; i<b.N; i++ {
+		Dump("id","int,string,bool",integer,words,boolean)
+	}
+}
+	
+
