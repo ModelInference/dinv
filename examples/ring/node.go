8c0f10b55907384a2292211c8b6292dc2a11dc15
Hendrik Cech <hendrik.cech@gmail.com>
2016-10-03 16:15:21 -0700

    Add ring example

---
+++ examples/ring/node.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bitbucket.org/bestchai/dinv/dinvRT"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"github.com/arcaneiceman/GoVector/capture"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync/atomic"
+	"syscall"
+)
+
+var (
+	counter  int64 = 0
+	neighbor net.Conn
+	logger   *log.Logger
+)
+
+func main() {
+	port := flag.Int("port", 0, "port which this node listens on")
+	neighborPort := flag.Int("neighbor", 0, "port neighbor node listens on")
+
+	flag.Parse()
+
+	if *port == 0 || *neighborPort == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	nodeID := "[N" + strconv.Itoa(*port) + "]"
+
+	logger = log.New(os.Stdout, nodeID, log.Lshortfile)
+
+	listenAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(*port))
+	printErrAndExit(err)
+	neighborAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(*neighborPort))
+	printErrAndExit(err)
+
+	listener, err := net.ListenUDP("udp", listenAddr)
+	printErrAndExit(err)
+	defer listener.Close()
+
+	neighbor, err = net.DialUDP("udp", nil, neighborAddr)
+	printErrAndExit(err)
+
+	logger.Printf("listening on %s, neighbor is %s", listenAddr.String(), neighborAddr.String())
+
+	go func() {
+		for {
+			buf := make([]byte, 8)
+			// _, err := listener.Read(buf)
+			_, err := capture.Read(listener.Read, buf)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			val, _ := binary.Varint(buf)
+
+			logger.Printf("received %v (%d)\n", buf, val)
+
+			if val > counter {
+				atomic.StoreInt64(&counter, val)
+				logger.Printf("received update: counter now at %d", atomic.LoadInt64(&counter))
+
+				dinvRT.Dump(nodeID, "counter", atomic.LoadInt64(&counter))
+				if err = forward(); err != nil {
+					fmt.Println(err)
+					continue
+				}
+			} else {
+				logger.Printf("received update %d, but counter already at %d\n", val,
+					atomic.LoadInt64(&counter))
+			}
+		}
+	}()
+
+	// hook for bash script
+	sigusr1 := make(chan os.Signal, 1)
+	signal.Notify(sigusr1, syscall.SIGUSR1)
+
+	for {
+		<-sigusr1
+		atomic.AddInt64(&counter, 1)
+		logger.Printf("signal received: counter increased to %d", atomic.LoadInt64(&counter))
+		if err := forward(); err != nil {
+			logger.Println(err)
+		}
+	}
+}
+
+func forward() (err error) {
+	msg := make([]byte, 8)
+	binary.PutVarint(msg, counter)
+	_, err = capture.Write(neighbor.Write, msg)
+	return
+}
+
+func printErrAndExit(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ examples/ring/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+# http://unix.stackexchange.com/a/145654
+exec &> >(tee -a "run.output")
+
+N=3
+M=10
+
+function runSystem {
+    echo "run system with $N nodes, and $M counter increments"
+
+    nodes=()
+    for n in $(seq 1 $N); do
+        # start nodes; wrap around at the end
+        ./ring -port "800$n" -neighbor "800$((n % N + 1))" &
+        nodes[$((n-1))]="$!"
+    done
+
+    for m in $(seq 1 $M); do
+        # select one node after the other
+        pid="${nodes[$((m % N))]}"
+        echo "sending SIGUSR1 to "
+        kill -SIGUSR1 "${nodes[$((m % N))]}"
+        sleep 0.2
+    done
+
+    # terminate all nodes
+    for job in $(jobs -p); do
+        kill $job
+    done
+}
+
+
+if [ "$1" != "" ]; then
+    N="$1"
+fi
+if [ "$2" != "" ]; then
+    M="$2"
+fi
+
+pushd "$(dirname "$0")"
+
+# ../lib.sh clean "failed"
+# ../lib.sh installDinv
+# GoVector -dir "client"
+# dinv -i -file "client/client.go"
+# GoVector -dir "server"
+# dinv -i -file "server/server.go"
+go build
+runSystem
+# ../lib.sh runLogMerger
+# ../lib.sh runDaikon
+# ../lib.sh clean
+
+popd
