7a2ea3815d0b2cebfb0630059b5cb8c04e75b944
Wanton Solutions <stewbertgrant@gmail.com>
2017-04-05 13:31:09 -0700

    added benchmark client to azure

--- experiements/etcd/azure/blast.sh
+++ experiements/etcd/azure/blast.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #./blast.sh words.txt serverIpPort command id
+# $1 textfile to put and get from the server
+# $2 serverIpPort
+# $3 The command to execute (etcd client binary)
+# $4 unique Id this client marks files with
 echo "starting azure client"
 lout=latency$4.txt
 echo "" > $lout
 i=0
 for word in $(<$1)
 do
+    #measure the latency in miliseconds
     a=$(($(date +%s%N)/1000000))
     ETCDCTL_API=3 $3 --endpoints=$2:2379 put $i "$word"
     b=$(($(date +%s%N)/1000000))
--- experiements/etcd/azure/measure.sh
+++ experiements/etcd/azure/measure.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # measure sets up a bunch of measuring clients
-# measure.sh words.txt [server ip:port] runtime clients etcdcdlLoc
-# (newline) client
-#ex ./measure.sh /
+# $1 Text file to put and get on a server
+# $2 [server ip:port] running etcd
+# $3 the time the clients should run for
+# $4 number of clients
+# $5 etcd client exacutable location
+# $6 client script for measuring or just execution on the server
+# $7 The benchmark to run: options [YCSB-A, YCSB-B, PUT-THEN-GET,
+# PUT-AND-GET]
 
-#command line arguments
-# $1: 
-#
-#
-#
 #
 i=0
 CLIENTS=$4
 
 for (( i=0; i<CLIENTS; i++ ))
 do
-    $6 $1 $2 $5 $i &
+    $6 $1 $2 $5 $i $7 &
 done
 
 echo "RUNTIME $3"
